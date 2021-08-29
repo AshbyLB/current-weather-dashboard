bf3932bd6f48cc5ebce3ae62f67954ba70b69b14
Ashby Blakely <ashbyleeblakely@gmail.com>
2021-08-29 12:18:19 -0400

    Working on getting the cureent weather from the API through JS

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,12 @@
 var apiKey = "7a1b1735c614d0057d9ac28220d05d40";
 var searchedCity = "Charlotte";
+var currentTemp = document.getElementById("currentTemp");
+var currentHumidity = document.getElementById("currentHumidity");
+var currentUv = document.getElementById("currentUv");
+var currentWind = document.getElementById("currentWind");
+var searchButton = document.getElementById("button-addon2");
+var cityEl = document.getElementById("cityInput");
+var currentCity = document.getElementById("currentCity");
 
 
 
@@ -7,28 +14,21 @@ var searchedCity = "Charlotte";
 
 
 
-function getApi() {
+
+
+searchButton.addEventListener("click", function () {
     var requestUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + searchedCity + "&appid=" + apiKey;
-  
+
     fetch(requestUrl)
-      .then(function (response) {
-        return response.json();
-      })
-      .then(function (data) {
-        console.log(data)
-        for (var i = 0; i < data.length; i++) {
-        
-  
-          link.textContent = data[i].daily.temp.day;
-          link.textContent = data[i].daily.humidity;
-          link.textContent = data[i].current.wind_speed;
-          link.textContent = data[i].current.uvi;
-          
-          tableData.appendChild(link);
-          createTableRow.appendChild(tableData);
-          tableBody.appendChild(createTableRow);
-        }
-      });
-  }
-  
-  getApi();
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (data) {
+           
+            currentCity.textContent = data.name;
+            currentTemp.textContent = "Temp: " + data.main.temp;
+            currentHumidity.textContent = "Humidity: " + data.main.humidity;
+            currentWind.textContent = "Wind Speed: " + data.wind.speed;
+            currentUv.textContent = "UVI: " + data.current.uvi;
+        })
+});
